dde5554d2ae5b3cc898bdf30fdec3d6548258c86
therobgeorge <georg1rd@gmail.com>
2021-06-28 11:40:07 -0400

    starts user sign up action

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/users/Signup.vue";
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/users/signup",
+    name: "signup",
+    component: Signup,
   },
 ];
 
